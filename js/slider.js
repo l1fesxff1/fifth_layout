6b11ae53e02ca998f706f106c9694b76b824382c
l1fesxff1 <andriykoroliyk030@gmail.com>
2022-09-27 12:23:41 +0300

    header-banner

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+// $(function () {
+//     $('.first_slider').slick({
+//         slidesToShow: 3,
+//         slidesToScroll:3,
+//         dots: false,
+//         arrows: false,
+//         responsive: [
+//             {
+//                 breakpoint: 851,
+//                 settings: {
+//                     dots: true,
+//                     slidesToShow: 2,
+//                     slidesToScroll: 2,
+//                 }
+//             },
+//
+//             {
+//                 breakpoint: 601,
+//                 settings: {
+//                     dots: true,
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                 }
+//             },
+//
+//         ]
+//     });
+// });
