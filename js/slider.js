df1cd2dbbddac6e7d80319a8983307761fa73a82
l1fesxff1 <andriykoroliyk030@gmail.com>
2022-10-03 11:19:33 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,18 @@
-// $(function () {
-//     $('.first_slider').slick({
-//         slidesToShow: 3,
-//         slidesToScroll:3,
-//         dots: false,
-//         arrows: false,
-//         responsive: [
-//             {
-//                 breakpoint: 851,
-//                 settings: {
-//                     dots: true,
-//                     slidesToShow: 2,
-//                     slidesToScroll: 2,
-//                 }
-//             },
-//
-//             {
-//                 breakpoint: 601,
-//                 settings: {
-//                     dots: true,
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                 }
-//             },
-//
-//         ]
-//     });
-// });
+$(function () {
+    $('.slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        infinite: false,
+        responsive:[
+            {
+                breakpoint:877,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
+});
